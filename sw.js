c981db731d3dcf5b701b40ebc9dd07f9dce4072b
lhpoicq <152121310+lhpoicq@users.noreply.github.com>
2025-12-19 15:34:04 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+
+const CACHE_NAME = 'xicheng-star-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=ZCOOL+KuaiLe&family=Noto+Sans+SC:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
